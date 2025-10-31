6a779b009bf80786fe3cda15f971566e0d8c81b9
Andres Felipe Quimbayo Trujillo <andres.quimbayo@thomasgreg.com>
2025-10-30 22:36:18 -0500

    Se implementan escenarios para sprint2

--- cypress/e2e/celeris-sprint1/adminEtapaProcElecto.cy.js
+++ cypress/e2e/celeris-sprint1/adminEtapaProcElecto.cy.js
@@ -3,7 +3,7 @@ describe('58289 Administrar Etapa del Proceso electoral - Consulta de etapa proc
         allure.epic("Sprint 1");
         allure.feature("Administrar Etapa del Proceso Electoral");
         cy.visit(Cypress.env('urlBase'))
-        cy.screenshot('Inicio de sesión', { capture: 'runner' });
+        cy.screenshot('Paso 1 Inicio de sesión', { capture: 'runner' });
         cy.get('input[placeholder="Usuario"]').type(Cypress.env('user'))
         cy.get('input[placeholder="Contraseña"]').type(Cypress.env('pass'))
         cy.wait(5000)
@@ -12,32 +12,32 @@ describe('58289 Administrar Etapa del Proceso electoral - Consulta de etapa proc
     })
     it('CP01 Acceso a la pantalla "Administrar Etapa del Proceso Electoral', () => {
         cy.contains('Administrar Etapa Proceso Electoral').click()
-        cy.screenshot('Administrar etapa', { capture: 'runner' });
+        cy.screenshot('Paso 2 Administrar etapa', { capture: 'runner' });
         cy.url().should('eq', 'https://celerisawsqa.tps.net.co/dashboard/parameterization/manage-stages');
 
     })
     it('CP02 Visualización inicial de la grilla y sección de totales', () => {
         cy.contains('Administrar Etapa Proceso Electoral').click()
         cy.url().should('eq', 'https://celerisawsqa.tps.net.co/dashboard/parameterization/manage-stages');
-        cy.screenshot('Administrar etapa', { capture: 'runner' });
+        cy.screenshot('Paso 2 Administrar etapa', { capture: 'runner' });
         cy.get('.relative.z-10.text-h-1.font-title.font-bold').should('be.visible')
 
     })
     it('CP03 Validación Búsqueda de etapas (consultas exitosas, sin resultados y campo obligatorio)', () => {
         cy.contains('Administrar Etapa Proceso Electoral').click()
         cy.url().should('eq', 'https://celerisawsqa.tps.net.co/dashboard/parameterization/manage-stages');
-        cy.screenshot('Administrar etapa', { capture: 'runner' });
+        cy.screenshot('Paso 2 Administrar etapa', { capture: 'runner' });
         cy.get('input[placeholder="Nombre etapa"]').dblclick({force: true})
         cy.get('input[placeholder="Nombre etapa"]').type('Nuevo')
         cy.contains('button', 'Consultar').click()
         cy.get('.text-sm.text-gray-500.mt-2.px-4.font-paragraph').should('be.visible')
-        cy.screenshot('Consulta', { capture: 'runner' });
+        cy.screenshot('Paso 3 Consulta', { capture: 'runner' });
         cy.reload()
         cy.get('input[placeholder="Nombre etapa"]').dblclick({force: true})
         cy.get('input[placeholder="Nombre etapa"]').type('Prueba')
         cy.contains('button', 'Consultar').click()
         cy.contains('td', 'A6-prueba').should('contain.text', 'A6-prueba')
-        cy.screenshot('Consulta', { capture: 'runner' });
+        cy.screenshot('Paso 4 Consulta', { capture: 'runner' });
 
     })
     it('CP04 Consulta exitosa con filtro aplicado', () => {
@@ -46,7 +46,7 @@ describe('58289 Administrar Etapa del Proceso electoral - Consulta de etapa proc
         cy.get('input[placeholder="Nombre etapa"]').dblclick({force: true})
         cy.get('input[placeholder="Nombre etapa"]').type('Prueba')
         cy.contains('button', 'Consultar').click()
-        cy.screenshot('Consulta exitosa', { capture: 'runner' });
+        cy.screenshot('Paso 2 Consulta exitosa', { capture: 'runner' });
         cy.contains('td', 'Prueba').click()
 
     })
@@ -56,18 +56,21 @@ describe('58289 Administrar Etapa del Proceso electoral - Consulta de etapa proc
         cy.get('input[placeholder="Nombre etapa"]').dblclick({force: true})
         cy.get('input[placeholder="Nombre etapa"]').type('Consulta')
         cy.contains('button', 'Limpiar').click()
-        cy.screenshot('Limpiar', { capture: 'runner' });
+        cy.screenshot('Paso 2 Limpiar', { capture: 'runner' });
     })
     it('CP06 Acciones en la grilla, creación y log de auditoría', () => {
         cy.contains('Administrar Etapa Proceso Electoral').click()
         cy.url().should('eq', 'https://celerisawsqa.tps.net.co/dashboard/parameterization/manage-stages');
+        cy.screenshot('Paso 2 Administrar etapa', { capture: 'runner' });
         cy.get('input[placeholder="Nombre etapa"]').dblclick({force: true})
         cy.get('input[placeholder="Nombre etapa"]').type('Prueba')
         cy.contains('button', 'Consultar').click()
         cy.get('svg.fa-pen-to-square').first().click();
+        cy.screenshot('Paso 3 Editar etapa', { capture: 'runner' });
         cy.get('h2.text-lg.font-title.font-medium').should('be.visible').and('contain.text','Editar Etapa Del Proceso')
         cy.contains('button', 'Cancelar').click()
         cy.get('svg.fa-trash').first().click();
+        cy.screenshot('Paso 4 Mensaje de eliminación', { capture: 'runner' });
         cy.get('p.text-sm.text-gray-500').should('be.visible').and('contain.text','Está seguro de que desea eliminar la etapa')
     })
 })
--- cypress/e2e/celeris-sprint2/adminActiviConsul.cy.js
+++ cypress/e2e/celeris-sprint2/adminActiviConsul.cy.js
@@ -7,12 +7,12 @@ describe('58307 : Administrar Actividades - Consulta de Actividades', () => {
         cy.get('input[placeholder="Contraseña"]').type(Cypress.env('pass'))
         cy.wait(5000)
         cy.get('button[type="submit"]').contains('Ingresar').dblclick({force: true})
-        cy.screenshot('Login exitoso', { capture: 'runner' });
+        cy.screenshot('Paso 1 Login exitoso', { capture: 'runner' });
         cy.url().should('eq', 'https://celerisawsqa.tps.net.co/dashboard')
     })
     it('CP1_Acceso a la pantalla únicamente perfil superadministrador', () => {
         cy.contains('Administrar Actividades para una Etapa').click()
-        cy.screenshot('Administrar etapa', { capture: 'runner' });
+        cy.screenshot('Paso 2 Administrar etapa', { capture: 'runner' });
         cy.url().should('eq', 'https://celerisawsqa.tps.net.co/dashboard/parameterization/manage-activities');
 
     })
