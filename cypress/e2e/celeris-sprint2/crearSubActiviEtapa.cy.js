1e73ae4e2638517b74b4c51d6f305ea68acf77e1
Andres Felipe Quimbayo Trujillo <andres.quimbayo@thomasgreg.com>
2025-11-04 15:57:10 -0500

    Se agregan scripts HU58309

--- cypress/e2e/celeris-sprint2/crearSubActiviEtapa.cy.js
+++ cypress/e2e/celeris-sprint2/crearSubActiviEtapa.cy.js
@@ -16,4 +16,27 @@ describe('58309 : Creación de Actividad para una etapa', () => {
         cy.get('.bg-linear-90.button.cursor-pointer.duration-300.flex.flex-row.font-paragraph.font-semibold.from-primary-500').click()
         cy.screenshot('Paso 2 Crear Sub-Actividad', { capture: 'runner' });
     })
+    it('CP02 - Validar la estructura de la pantalla', () => {
+        cy.get('.text-h2.text-lg.font-medium').should('be.visible')
+        cy.get('.relative.z-10.text-h-1.font-title.font-bold').should('be.visible')
+        cy.get('select').first().should('be.visible')
+        cy.get('select').eq(1).should('be.visible')
+        cy.contains('button','Consultar').click()
+        cy.get('svg.fa-pen-to-square').first().should('be.visible');
+        cy.get('svg.fa-trash').first().should('be.visible');
+        cy.get('svg.fa-magnifying-glass').first().should('be.visible');
+        cy.get('.bg-linear-90.button.cursor-pointer.duration-300.flex.flex-row.font-paragraph.font-semibold.from-primary-900').should('contain.text','Crear Actividad')
+        cy.get('.bg-linear-90.button.cursor-pointer.duration-300.flex.flex-row.font-paragraph.font-semibold.from-primary-500.h-full').should('contain.text','Crear Sub Actividad')
+        cy.get('.bg-linear-90.button.cursor-pointer.duration-300.flex.flex-row.font-paragraph.font-semibold.from-primary-500.h-full').click()
+        cy.screenshot('Paso 2 Crear Sub-Actividad', { capture: 'runner' });
+        cy.get('select.block.w-full.cursor-pointer').eq(4).should('be.visible')
+        cy.get('select.block.w-full.cursor-pointer').eq(5).should('be.visible')
+        cy.get('input[placeholder="Texto"]').should('be.visible')
+        cy.get('.flex.transition-all.text-center.duration-300.ease-in-out.text-wrap.break-words.outline-1').should('be.visible')
+        cy.get('select.block.w-full.cursor-pointer.text-sm.font-paragraph.text-normal.appearance-none.py-2.px-3.pr-10.bg-white.rounded-full').last().should('be.visible')
+    })
+    it('CP03_Validar estructura del formulario de creación de actividad', () => {
+        cy.get('.bg-linear-90.button.cursor-pointer.duration-300.flex.flex-row.font-paragraph.font-semibold.from-primary-500').click()
+        cy.screenshot('Paso 2 Crear Sub-Actividad', { capture: 'runner' });
+    })
 })
