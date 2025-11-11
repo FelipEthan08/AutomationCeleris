e1ff7498441fb6abeed42a942c3a6de17604df45
Andres Felipe Quimbayo Trujillo <andres.quimbayo@thomasgreg.com>
2025-11-11 16:12:15 -0500

    Se ajusta package.json

--- cypress.config.js
+++ cypress.config.js
@@ -8,23 +8,8 @@ export default defineConfig({
         setupNodeEvents(on, config) {
             // Inicializa el escritor de resultados de Allure
             allureWriter(on, config);
-
-            // Agregar flags al navegador antes de lanzarlo
-            on("before:browser:launch", (browser = {}, launchOptions) => {
-                if (browser.family === "chromium") {
-                    launchOptions.args.push(
-                        "--disable-gpu",
-                        "--no-sandbox",
-                        "--disable-dev-shm-usage",
-                        "--remote-allow-origins=*"
-                    );
-                }
-                return launchOptions;
-            });
-
             return config;
         },
-
         retries: {
             runMode: 4,
             openMode: 1,
