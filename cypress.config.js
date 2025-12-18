7821048c660957afdff0a5a298383caa5cf76ecd
Andres Felipe Quimbayo <andres.quimbayo@thomasgreg.com>
2025-12-18 09:09:08 -0500

    Se ajusta codigo para ejecutar desde Git

--- cypress.config.js
+++ cypress.config.js
@@ -43,7 +43,7 @@
         env: {
             allure: true,
             allureResultsPath: "allure-results",
-            user: "1121212642",
+            user: "1990",
             pass: "Bogota.2025*",
             urlBase: "https://celerisawsqa.tps.net.co/auth/login",
         }
