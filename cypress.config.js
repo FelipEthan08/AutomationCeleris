2c9afc2cb645591a95bb530a7592ad7c3786dd35
Andres Felipe Quimbayo <andres.quimbayo@thomasgreg.com>
2025-12-17 22:36:26 -0500

    Se ajusta codigo para ejecutar desde Git

--- cypress.config.js
+++ cypress.config.js
@@ -1,50 +1,50 @@
-import { defineConfig } from "cypress";
-import allureWriter from "@shelex/cypress-allure-plugin/writer";
-import fs from "fs";
-import path from "path";
+    import { defineConfig } from "cypress";
+    import allureWriter from "@shelex/cypress-allure-plugin/writer";
+    import fs from "fs";
+    import path from "path";
 
-export default defineConfig({
-    projectId: "s3u5ws",
+    export default defineConfig({
+        projectId: "s3u5ws",
 
-    e2e: {
-        pageLoadTimeout: 280000,
-        setupNodeEvents(on, config) {
-            // Inicializa el escritor de resultados de Allure
-            allureWriter(on, config);
+        e2e: {
+            pageLoadTimeout: 280000,
+            setupNodeEvents(on, config) {
+                // Inicializa el escritor de resultados de Allure
+                allureWriter(on, config);
 
-            // Task para crear un archivo dinámico
-            on("task", {
-                crearArchivo({ nombreArchivo, contenido }) {
-                    const filePath = path.join("cypress", "fixtures", nombreArchivo);
-                    fs.writeFileSync(filePath, contenido);
-                    return null; // siempre retornar algo
-                },
-            });
+                // Task para crear un archivo dinámico
+                on("task", {
+                    crearArchivo({ nombreArchivo, contenido }) {
+                        const filePath = path.join("cypress", "fixtures", nombreArchivo);
+                        fs.writeFileSync(filePath, contenido);
+                        return null; // siempre retornar algo
+                    },
+                });
 
-            return config;
+                return config;
+            },
+
+            retries: {
+                runMode: 4,
+                openMode: 3,
+            },
+
+            experimentalRunAllSpecs: true,
         },
 
-        retries: {
-            runMode: 4,
-            openMode: 3,
-        },
+        screenshotsFolder: "cypress/screenshots",
+        viewportWidth: 1920,
+        viewportHeight: 1080,
+        defaultCommandTimeout: 20000,
+        screenshotOnRunFailure: false,
+        chromeWebSecurity: false,
+        scrollBehavior: false,
 
-        experimentalRunAllSpecs: true,
-    },
-
-    screenshotsFolder: "cypress/screenshots",
-    viewportWidth: 1920,
-    viewportHeight: 1080,
-    defaultCommandTimeout: 20000,
-    screenshotOnRunFailure: false,
-    chromeWebSecurity: false,
-    scrollBehavior: false,
-
-    env: {
-        allure: true,
-        allureResultsPath: "allure-results",
-        user: "1121212642",
-        pass: "Bogota.2025*",
-        urlBase: "https://celerisawsqa.tps.net.co/auth/login",
-    }
-});
+        env: {
+            allure: true,
+            allureResultsPath: "allure-results",
+            user: "1121212642",
+            pass: "Bogota.2025*",
+            urlBase: "https://celerisawsqa.tps.net.co/auth/login",
+        }
+    });
